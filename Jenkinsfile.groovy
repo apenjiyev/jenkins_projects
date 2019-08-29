b3f3738108c5862cb88fd5daa445a76ac76c5637
apenjiyev <azatpenji@gmail.com>
2019-08-28 17:16:34 -0700

    updated the repo with updated host IP

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,7 +11,7 @@ node {
         sh "ssh ec2-user@${ENVIR} sudo rm -rf /home/ec2-user/flask-examples"
     }
     stage("Pull Repo"){
-        sh "ssh ec2-user@${ENVIR} git clone https://github.com/miguelgrinberg/flask-examples.git"
+        sh "ssh ec2-user@${ENVIR} git clone https://github.com/apenjiyev/flask-examples.git"
     }
     stage("Install requirements"){
         //sh "virtualenv /tmp/venv"
